27a96e428760e89ae005e74f5e64f9e8c36e8332
YavuzOKTAY <yavuzdark34@gmail.com>
2017-12-30 17:12:32 +0300

    Initial commit

---
+++ app/src/main/java/com/yavuzoktay/simpleokhttp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.yavuzoktay.simpleokhttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mtextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mtextView=findViewById(R.id.textView);
+
+        OkHttpClient client=new OkHttpClient();
+
+        String url="https://reqres.in/api/users?page=2";
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.getStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()){
+                    final String myResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mtextView.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
